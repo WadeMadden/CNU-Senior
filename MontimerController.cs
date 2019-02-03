26c422aca59838330baf1238e26f1904fd91531b
WadeMadden <wade.madden.15@cnu.edu>
2019-02-02 21:42:49 -0500

    Initial Commit

---
+++ MontimerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MontimerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initial commit
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
